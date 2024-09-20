9f1a5d836e91243f7e6a183543967dc7726e5170
Baran Akıllı <akillibaran@hotmail.com>
2024-09-21 02:19:59 +0300

    added exclamation mark!

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -144,7 +144,7 @@ const OrderItem = ({ orderProducts, storeProducts, customer, isAbroad, cargoTrac
                   className="bg-blue-500 w-full text-xl text-white px-4 py-2 rounded-lg hover:bg-blue-600"
                   onClick={handlePrintLabelClick}
                 >
-                  Etiket Yazdır
+                  Etiket Yazdır!
                 </button>
               </div>
             ) : (
